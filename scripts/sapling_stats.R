be579a5b7740e4b6cec303551a9c539a4279a94a
Deborah G Nemens <dnemens@uw.edu>
2018-01-23 18:02:24 -0800

    up

--- scripts/sapling_stats.R
+++ scripts/sapling_stats.R
@@ -1,7 +1,15 @@
 #extracts summary stats for conifer seedlings and saplings
 
-center <- read_csv("C:/Users/dnemens/Dropbox/CBO/Data/data sheets/center sub plot.csv")
-midstory <- read_csv("C:/Users/dnemens/Dropbox/CBO/Data/data sheets/midstory.csv")
+center <- read.csv("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/center sub plot.csv")
+abco <- center %>%
+  filter(Spp == "ABCO" | Spp == "PSME") %>%
+  group_by(ht) %>%
+  group_by(plot) %>%
+  summarize(abco.density = length(which(Spp == "ABCO")*63.69), psme.density = length(which(Spp == "PSME")*63.69)
+
+
+
+midstory <- read.csv("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/midstory.csv")
 
 library(dplyr)
 
