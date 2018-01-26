7e2e127475f1e4a2ef08235488716962a291e2b2
dnemens <35116826+dnemens@users.noreply.github.com>
2018-01-26 14:45:27 -0800

    update

--- scripts/sapling_stats.R
+++ scripts/sapling_stats.R
@@ -1,26 +1,94 @@
 #extracts summary stats for conifer seedlings and saplings
-
-center <- read.csv("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/center sub plot.csv")
 library(tidyr) #for pipe operator?
 library(dplyr)
-library(MASS)
 
-#calculates density for both abco and psme using center sub-plot data
-abco <- center %>%
+center <- read.csv("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/center sub plot.csv")
+
+#calculates sapling (>50cm) density using center sub-plot data
+density.c <- center %>%
   filter(Spp == "ABCO" | Spp == "PSME") %>%
-  group_by(ht) %>%
+  filter(ht>=3) %>%
   group_by(plot) %>%
-  summarize(abco.density = length(which(Spp == "ABCO")*63.69), psme.density = length(which(Spp == "PSME")*63.69))
+  summarize(abco.density = length(which(Spp == "ABCO")), psme.density = length(which(Spp == "PSME")))
 
-ab.s <- sum (abco$abco.density)
-ps.s <-  sum (abco$psme.density)
+  density.c <- mutate(density.c, den.ha.abco = density.c$abco.density*177, den.ha.psme = psme.density*177)
+  
+mean(density.c$den.ha.abco)
+mean(density.c$den.ha.psme)
+sd(density.c$den.ha.abco)
+sd(density.c$den.ha.psme)
 
+#calculates sapling frequency using center sub-plot data
+abcos <- density.c$abco.density
+length(abcos[which(abcos>0)])
+psmes <- density.c$psme.density
+length(psmes[which(psmes>0)])
+
+#calculates seedling (<50cm) density using center sub-plot data
+density.c.s <- center %>%
+  filter(Spp == "ABCO" | Spp == "PSME") %>%
+  filter(ht<3) %>%
+  group_by(plot) %>%
+  summarize(abco.density = length(which(Spp == "ABCO")), psme.density = length(which(Spp == "PSME")))
+
+density.c.s <- mutate(density.c.s, den.ha.abco = abco.density*177, den.ha.psme = psme.density*177)
+
+mean(density.c.s$den.ha.abco)
+sd(density.c.s$den.ha.abco)
+mean(density.c.s$den.ha.psme)
+sd(density.c.s$den.ha.psme)
+
+#calculates seedling frequency using center sub-plot data
+abcos <- density.c.s$abco.density
+length(abcos[which(abcos>0)])
+psmes <- density.c.s$psme.density
+length(psmes[which(psmes>0)])
+
+###################same as above with focal oak midstory data#######################
 midstory <- read.csv("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/midstory.csv")
 
-#caluculates 
-abco.m <- midstory %>%
+#caluculates sapling density using focal oak midstory data
+#number of stems per plot 
+density.m <- midstory %>%
   filter(spp == "ABCO" | spp == "PSME") %>%
-  summarize(abco.density = length(which(spp == "ABCO")*63.69), psme.density = length(which(spp == "PSME")*63.69))
-  
-  group_by(Plot) %>%
-  summarize(abco.density = sum((tree.num)[which(spp == "ABCO")]*63.69), psme.density = sum((tree.num)[which(spp == "PSME")]*63.69))
+  group_by(Plot) %>% 
+  filter(tree.ht.class>=3) %>% 
+  summarize(abco.density = sum((tree.num)[which(spp == "ABCO")]), psme.density = sum((tree.num)[which(spp == "PSME")]))
+#number of stems per ha
+density.m <- mutate(density.m, den.ha.abco = density.m$abco.density*63.69, den.ha.psme = density.m$psme.density*63.69)
+
+mean(density.m$den.ha.abco)
+sd(density.m$den.ha.abco)
+mean(density.m$den.ha.psme)
+sd(density.m$den.ha.psme)
+
+#calculates sapling frequency using focal oak midstory data
+abcos <- density.m$abco.density
+length(abcos[which(abcos>0)])
+psmes <- density.m$psme.density
+length(psmes[which(psmes>0)])
+
+###############################################
+#caluculates seedling density using focal oak midstory data
+#number of stems per plot 
+density.m.s <- midstory %>%
+  filter(spp == "ABCO" | spp == "PSME") %>%
+  group_by(Plot) %>% 
+  filter(tree.ht.class<3) %>% 
+  summarize(abco.density = sum((tree.num)[which(spp == "ABCO")]), psme.density = sum((tree.num)[which(spp == "PSME")]))
+#number of stems per ha
+density.m.s <- mutate(density.m.s, den.ha.abco = density.m.s$abco.density*63.69, den.ha.psme = density.m.s$psme.density*63.69)
+
+mean(density.m.s$den.ha.abco)
+sd(density.m.s$den.ha.abco)
+mean(density.m.s$den.ha.psme)
+sd(density.m.s$den.ha.psme)
+
+#calculates seedling frequency using focal oak midstory data
+abcos <- density.m.s$abco.density
+length(abcos[which(abcos>0)])
+psmes <- density.m.s$psme.density
+length(psmes[which(psmes>0)])
+
+
+
