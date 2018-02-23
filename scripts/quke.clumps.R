a41c2a32501eaaa477326099cefab3e26d285af7
dnemens <35116826+dnemens@users.noreply.github.com>
2018-02-22 16:35:39 -0800

    ongoing

--- scripts/quke.clumps.R
+++ scripts/quke.clumps.R
@@ -25,14 +25,14 @@ sd(quke.clumps$clumps.ha)
 #loads csv of sprout response data 
 sprout <- read.csv ("C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/focaloak.csv")
 
+#replaces NAs with 0's
+sprout[is.na(sprout)] <- 0
+
 #function for calculating SE
 se <- function(x) sqrt(var(x)/length(x))
 
 #sprout <- mutate(sprout, ba.m2ha=chips.live^2*.00007854)
 sprout2 <- mutate(sprout, ba.cm2.C=(((chips.live/2)^2)*pi), ba.cm2.S=(((storrie.live/2)^2)*pi))
-
-#replaces NAs with 0's
-sprout2[is.na(sprout2)] <- 0
 
 #calculates total basal area of each sprout clump
 sprout3 <- sprout2 %>%
@@ -50,17 +50,18 @@ sprout4 <- mutate(sprout4, bah.S=(BA.S*clumps.ha), bah.C=(BA.C*clumps.ha))
 #calculates sprouts per ha
 sprout4 <- mutate(sprout4, sph.S=(stems.S*clumps.ha), sph.C=(stems.C*clumps.ha))
 
+write.csv(sprout4, file = "C:/Users/dnemens/Dropbox/CBO/black-oak/data sheets/sprout_calc.csv")
+
 #produces summary stats 
 #for Storrie sprouts
 quke.S <- sprout4 %>%
   filter(stems.S>0) %>%
-  summarise(mean.S.b = mean(bap.S), se.S.b = se(bap.S), sd.S.b = sd(bap.S), mean.S = mean(sph.S), sd.S = sd(sph.S), se.S = se(sph.S)) 
-#should I filter out the 0's?  probably...
+  summarise(mean.S.b = mean(BA.S), se.S.b = se(BA.S), mean.S = mean(stems.S), se.S=se(stems.S), mean.Sh = mean(sph.S), se.Sh = se(sph.S)) 
 
 #for Chips sprouts
 quke.C <- sprout4 %>%
   filter(stems.C>0) %>%
-  summarise(mean.C = mean(sph.C), sd.C = sd(sph.C), se.C=se(sph.C))
+  summarise(mean.C.b = mean(BA.C), se.C.b = se(BA.C), mean.C = mean(stems.C), se.Cc=se(stems.C), mean.Ch = mean(sph.C), se.Ch=se(sph.C))
 
 ########################################################################################
 #caluculates a stems/ha for each Storrie severity category (1-4)
